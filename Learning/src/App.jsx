915358eccf597a0c67396190b2e03086cf3561e6
Skanda Prasad <skandaprasad595@gmail.com>
2025-03-10 21:47:14 +0530

    Done Entry thing

--- Learning/src/App.jsx
+++ Learning/src/App.jsx
@@ -1,7 +1,10 @@
 import Header from "../Components/Header"
-
+import Entry from "../Components/Entry"
 export default function App(){
   return (
-    <Header />
+    <>
+        <Header />
+        <Entry /> 
+    </>
   )
 }
