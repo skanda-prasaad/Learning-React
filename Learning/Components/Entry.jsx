e81838af9df1814a1d40cc9706135dbf5e2e0e3d
Skanda Prasad <skandaprasad595@gmail.com>
2025-03-11 20:12:34 +0530

    Completed-Travel

--- Learning/Components/Entry.jsx
+++ Learning/Components/Entry.jsx
@@ -1,23 +1,19 @@
-import marker from "../src/assets/marker.png"
+import marker from "../src/assets/marker.png";
 
-export default function Entry(){
-    return(
-        <article className="entry">
-            <div className="image-container">
-                <img className="entry-image" src="https://scrimba.com/links/travel-journal-japan-image-url" alt="Japan" />
-            </div>
-            <div className="info">
-            <img 
-                    className="marker"
-                    src={marker}
-                    alt="map marker icon"
-                />
-                <span className="country">Japan</span>
-                <a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu">View on Google Maps</a>
-                <h2 className="entry-title">Mount Fuji</h2>
-                <p className="trip-dates">12 Jan, 2021 - 24 Jan, 2021</p>
-                <p className="entry-text">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
-            </div>
-        </article>
-    )
+export default function Entry(props) {
+  return (
+    <article className="entry">
+      <div className="image-container">
+        <img className="entry-image" src={props.entry.img.src} alt={props.entry.img.alt} />
+      </div>
+      <div className="info">
+        <img className="marker" src={marker} alt="map marker icon" />
+        <span className="country">{props.entry.country}</span>
+        <a href={props.entry.googleLink}>View on Google Maps</a>
+        <h2 className="entry-title"> {props.entry.name} </h2>
+        <p className="trip-dates"> {props.entry.dates} </p>
+        <p className="entry-text"> {props.entry.text} </p>
+      </div>
+    </article>
+  );
 }
--- Learning/src/App.jsx
+++ Learning/src/App.jsx
@@ -1,10 +1,17 @@
 import Header from "../Components/Header"
 import Entry from "../Components/Entry"
+import datas from "./data.js"
 export default function App(){
+  const Entrycontent = datas.map((data) => {
+    return <Entry 
+      key = {data.id}
+      entry = {data}
+    />
+  })
   return (
     <>
         <Header />
-        <Entry /> 
+       {Entrycontent}
     </>
   )
 }
