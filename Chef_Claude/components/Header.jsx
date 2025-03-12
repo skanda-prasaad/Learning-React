f2216e8d8faca617e02d51ad9058da6c74b96f18
Skanda Prasad <skandaprasad595@gmail.com>
2025-03-12 14:45:12 +0530

    started-chef_claude

---
+++ Chef_Claude/components/Header.jsx
@@ -0,0 +1,11 @@
+import cheficon from "../src/assets/chef.png"
+
+export default function Header()
+{
+    return (
+        <header>
+            <img src={cheficon} alt="" />
+            <h1></h1>
+        </header>
+    )
+}
