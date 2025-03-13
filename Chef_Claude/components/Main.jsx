46e1ed958483039b15a69f36f3deaec8c69165e6
Skanda Prasad <skandaprasad595@gmail.com>
2025-03-13 23:14:04 +0530

    commit

--- Chef_Claude/components/Main.jsx
+++ Chef_Claude/components/Main.jsx
@@ -1,14 +1,33 @@
+import React  from "react"
+
 export default function Main(){
+
+    const [ingredient , setIngredient] = React.useState([])
+
+    const ingredientList = ingredient.map((ing) => {
+        return <li key={ing}>{ing}</li>
+    })
+
+    function addIngredient(formData){
+        const inpValue = formData.get("ingredient");
+        setIngredient(
+            (prev) => [...prev, inpValue]
+        )
+    }
     return (
         <main>
-            <form className="add-ingredient-form">
+            <form action = {addIngredient} className="add-ingredient-form">
                 <input 
                     type="text"
                     placeholder="e.g. oregano"
                     aria-label="Add ingredient"
+                    name="ingredient"
                 />
                 <button>Add ingredient</button>
             </form>
+            <ul>
+                {ingredientList}
+            </ul>
         </main>
     )
 }
